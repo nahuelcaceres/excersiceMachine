98d7bad200eb0c0a52d66e073104196e7ff43999
nahuelcaceres <nahuecaceres@gmail.com>
2014-05-31 19:18:35 -0300

    commit

--- exercise-entity/src/main/java/ar/com/masch/exercise/learning/entity/base/BaseEntity.java
+++ exercise-entity/src/main/java/ar/com/masch/exercise/learning/entity/base/BaseEntity.java
@@ -11,7 +11,6 @@ import org.apache.commons.lang.builder.EqualsBuilder;
 @MappedSuperclass
 public class BaseEntity {
 
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID", unique = true, nullable = false)
