c9df493fbb495e6cf839462ee3bda5be0451dd13
masch <gmarioc@gmail.com>
2014-06-06 20:02:03 -0300

    Borrado de warnings.

--- exercise-entity/src/main/java/ar/com/masch/exercise/learning/entity/base/ExerciseRoutineEntity.java
+++ exercise-entity/src/main/java/ar/com/masch/exercise/learning/entity/base/ExerciseRoutineEntity.java
@@ -7,12 +7,12 @@ import javax.persistence.Table;
 @Entity(name= "exercise_routine")
 public class ExerciseRoutineEntity extends BaseEntity{
 	//TODO: hacer los getters y setters de las entidades
-	
-	private RoutineEntity routineEntity;
-	private ExerciseBaseEntity exerciseBaseEntity;
-
-	public ExerciseRoutineEntity () {
-		super();
-	}
+//	
+//	private RoutineEntity routineEntity;
+//	private ExerciseBaseEntity exerciseBaseEntity;
+//
+//	public ExerciseRoutineEntity () {
+//		super();
+//	}
 	
 }
